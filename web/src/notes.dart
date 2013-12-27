6e513d3f2082bf6f8cb554c7901643178c11c395
Ole Martin Gjersvik <olem@olem.org>
2013-12-27 08:09:40 +0100

    Added a create note feature.

--- web/src/notes.dart
+++ web/src/notes.dart
@@ -5,9 +5,26 @@ class Notes{
   
   final Server _server;
   
+  final DivElement _newNote = new DivElement();
+  final TextInputElement _textbox = new TextInputElement();
+  ButtonElement _create = new ButtonElement();
+  
   Notes(this._server){
     elem.className = 'notes';
+    _create.text = 'Create new note';
+    _create.onClick.listen((MouseEvent e){
+      if(e.button != 0 || _textbox.value == ''){
+        return;
+      }
+      add(new Note(_textbox.value,'',_server));
+      _textbox.value = '';
+    });
     
+    _newNote.className = 'newNote';
+    _newNote.append(_textbox);
+    _newNote.append(_create);
+    
+    elem.append(_newNote);
     _server.getJson('note').then((List notes){
       notes.forEach((Map note){
         add(new Note(note['name'],note['text'],_server));
