a9d960734e26fc1038d90ccd4965ef2f852d3fc1
Ole Martin Gjersvik <olem@olem.org>
2014-01-03 09:31:45 +0100

    Refractored note and note contoller

--- web/src/note_controller.dart
+++ web/src/note_controller.dart
@@ -10,15 +10,32 @@ class NoteController{
   final ButtonElement _create = new ButtonElement();
   
   NoteController(this._server, this._panels){
-    _newNote.onNewNote.listen((name){
-      _panels.add(new Note(name,'',_server));
-    });
+    _newNote.onNewNote.listen((name) => newNote(name,''));
     _panels.add(_newNote);
     
     _server.getJson('note').then((List notes){
-      notes.forEach((Map note){
-        _panels.add(new Note(note['name'],note['text'],_server));
-      });
+      notes.forEach((Map note) => newNote(note['name'],note['text']));
     });
+  }
+  
+  save(NotePanel note){
+    _server.put('note/${note.title}', note.note)
+      .then((_){
+        note.saveDone();
+      });
+  }
+  
+  delete(NotePanel note){
+    _server.delete('note/${note.title}')
+      .then((_){
+        _panels.remove(note);
+      });
+  }
+  
+  newNote(name,note){
+    var panel = new NotePanel(name,note);
+    panel.onSave.listen(save);
+    panel.onDelete.listen(delete);
+    _panels.add(panel);
   }
 }
---
+++ web/src/note_panel.dart
@@ -0,0 +1,49 @@
+part of ompa_html;
+
+class NotePanel extends Panel{
+  final String title;
+  
+  Stream<NotePanel> onSave;
+  Stream<NotePanel> onDelete;
+  
+  
+  ButtonElement _save = new ButtonElement();
+  ButtonElement _delete = new ButtonElement();
+  TextAreaElement _textbox =  new TextAreaElement();
+  
+  NotePanel(this.title, text): super(){
+    note = text;
+    
+    content.classes.add('note');
+    
+    var _title = new HeadingElement.h1();
+    _title.text = title;
+    content.append(_title);
+    content.append(_textbox);
+    
+    _save.text = 'Save';
+    _save.className = 'save';
+    onSave = _save.onClick
+        .where((MouseEvent e) => e.button == 0)
+          .map((_){
+            _save.text = 'Saveing...';
+            return this;
+          });
+    content.append(_save);
+    
+    _delete.text = 'Delete';
+    _delete.className = 'delete';
+    onDelete = _delete.onClick
+        .where((MouseEvent e) => e.button == 0)
+          .map((_){
+            _delete.text = 'Deleting';
+            return this;
+          });
+    content.append(_delete);
+  }
+  
+  String get note => _textbox.value;
+  set note(text) => _textbox.value = text;
+  
+  saveDone() => _save.text = 'Save';
+}
