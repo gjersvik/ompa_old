7adc4c6ac473ac32a6ddae6fa15df9e589393573
Ole Martin Gjersvik <olem@olem.org>
2014-01-03 12:34:30 +0100

    Added a top

--- web/src/success_panel.dart
+++ web/src/success_panel.dart
@@ -9,11 +9,16 @@ class SuccessPanel extends Panel{
     next.text = '>';
     next.className = 'next';
     
+    var date = new SpanElement();
+    date.text = '1. January 2014';
     
     var header = new DivElement();
+    header.className = 'top';
     header.append(prev);
+    header.append(date);
     header.append(next);
     
+    content.classes.add('success');
     content.append(header);
   }
 }
