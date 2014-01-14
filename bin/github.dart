b483e7a15ce3ed6d1d4e4b650add69471392ef51
Ole Martin Gjersvik <olem@olem.org>
2014-01-14 15:02:52 +0100

    GitHub Polling

--- bin/github.dart
+++ bin/github.dart
@@ -5,7 +5,8 @@ import 'dart:convert';
 class GitHub {
   var client = new HttpClient();
   var uri = Uri.parse('https://8f55e6b86df1a2f3a00a8cc046c3489438d7645f@api.github.com/users/gjersvik/events');
-  var etag = '';
+  var etag = null;
+  Duration pool =  new Duration(seconds: 60);
   
   GitHub(){
     client.userAgent = 'gjersvik';
@@ -13,14 +14,24 @@ class GitHub {
   
   poolEvents(){
     return client.getUrl(uri).then((HttpClientRequest request) {
+      if(etag != ''){
+        request.headers.add('If-None-Match', etag);
+      }
       return request.close();
     }).then((HttpClientResponse res) {
       print('${res.statusCode} ${res.reasonPhrase }');
       res.headers.forEach((name, values) => print('$name: ' + values.join(', ')));
-      UTF8.decodeStream(res).then(print);
       
-    }).then((_){
-      new Timer(new Duration(seconds: 60),poolEvents);
+      if(res.statusCode == 200){
+        etag = res.headers.value('etag');
+        pool = new Duration(seconds: int.parse(res.headers.value('x-poll-interval')));
+        return UTF8.decodeStream(res).then(print)
+            .then((_) => pool);
+      }else{
+        return pool;
+      }
+    }).then((Duration poll){
+      new Timer(poll, poolEvents);
     });
   }
 }
