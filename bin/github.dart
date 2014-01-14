c7bcbd2dd3a29f79a8bdf1c8f5a4c24f17c3cf07
Ole Martin Gjersvik <olem@olem.org>
2014-01-14 14:42:00 +0100

    Moved github to a class

--- bin/github.dart
+++ bin/github.dart
@@ -1,18 +1,31 @@
+import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
 
-main(){
-  var uri = Uri.parse('https://8f55e6b86df1a2f3a00a8cc046c3489438d7645f@api.github.com/users/gjersvik/events');
-  
+class GitHub {
   var client = new HttpClient();
-  client.userAgent = 'gjersvik';
-  client.getUrl(uri).then((HttpClientRequest request) {
-    return request.close();
-  })
-  .then((HttpClientResponse res) {
-    print('${res.statusCode} ${res.reasonPhrase }');
-    res.headers.forEach((name, values) => print('$name: ' + values.join(', ')));
-    UTF8.decodeStream(res).then(print);
-    
-  });
+  var uri = Uri.parse('https://8f55e6b86df1a2f3a00a8cc046c3489438d7645f@api.github.com/users/gjersvik/events');
+  var etag = '';
+  
+  GitHub(){
+    client.userAgent = 'gjersvik';
+  }
+  
+  poolEvents(){
+    return client.getUrl(uri).then((HttpClientRequest request) {
+      return request.close();
+    }).then((HttpClientResponse res) {
+      print('${res.statusCode} ${res.reasonPhrase }');
+      res.headers.forEach((name, values) => print('$name: ' + values.join(', ')));
+      UTF8.decodeStream(res).then(print);
+      
+    }).then((_){
+      new Timer(new Duration(seconds: 60),poolEvents);
+    });
+  }
+}
+
+main(){
+  var github = new GitHub();
+  github.poolEvents();
 }
