8ba1b83e312e617a604620998e670abe613eb32b
Ole Martin Gjersvik <olem@olem.org>
2014-01-19 21:28:40 +0100

    Add some ugly html.

--- web/src/tasks.dart
+++ web/src/tasks.dart
@@ -5,6 +5,11 @@ class Tasks {
   Tasks(Element parent){
     Element tasks = new DivElement();
     tasks.className = 'tasks';
+    tasks.appendHtml('<h1>Tasks</h1>');
+    tasks.appendHtml('<ul></ul>');
+    tasks.appendHtml('<input type="text"/><button>Add</button>');
+    
+    
     parent.append(tasks);
   }
 }
