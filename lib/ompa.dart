83dc2f0671c5cf8f198f2e509cb7d6b628ef1b76
Ole Martin Gjersvik <olem@olem.org>
2014-01-19 14:29:36 +0100

    Added a Task model.

--- lib/ompa.dart
+++ lib/ompa.dart
@@ -2,5 +2,8 @@ library ompa_common;
 
 import 'dart:convert';
 
+import 'package:bson/bson.dart';
+
 part 'src/model.dart';
 part 'src/success.dart';
+part 'src/task.dart';
---
+++ lib/src/task.dart
@@ -0,0 +1,47 @@
+part of ompa_common;
+
+class Task extends Model{
+  String name = '';
+  ObjectId _id;
+  
+  Task();
+
+  factory Task.formJson(json){
+    if(json is String){
+      json = JSON.decode(json);
+    }
+    var task = new Task();
+    task.name = json['name'];
+    if(json['_id'] != null){
+      task._id = ObjectId.parse(json['_id']);
+    }
+    return task;
+  }
+  
+  factory Task.formDb(Map doc){
+    var task = new Task();
+    task.name = doc['name'];
+    task._id = doc['_id'];
+    return task;
+  }
+  
+  Map toJson(){
+    var json = {'name' : name};
+    if(_id != null){
+      json['_id'] = _id.toHexString();
+    }
+    return json;
+  }
+  
+  String toString(){
+    return JSON.encode(this);
+  }
+  
+  Map toDb(){
+    var doc = {'name' : name};
+    if(_id != null){
+      doc['_id'] = _id;
+    }
+    return doc;
+  }
+}
