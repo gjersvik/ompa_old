fd747baaf69b4218f423a6f031719a9c6ded3074
Ole Martin Gjersvik <olem@olem.org>
2014-02-02 10:29:44 +0100

    Codeship now have the right aws cli.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -u -e;
 
-#Codeship don't have the ofisial aws instaled.
-pip install awscli
-
 #Upload client code.
 aws s3 sync ./build s3://ompa.olem.org --acl public-read  --delete
