3dde102fc2068eb1d12212a15a917f23bd10238d
Ole Martin Gjersvik <olem@olem.org>
2014-02-02 11:55:09 +0100

    fixed typo

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ aws s3 sync ./build s3://ompa.olem.org --acl public-read --delete
 
 #Create a distrebution
 mkdir dist
-dart --shapshot=dist/ompa.snapshot bin/ompa.dart
+dart --snapshot=dist/ompa.snapshot bin/ompa.dart
 ls -a dist
