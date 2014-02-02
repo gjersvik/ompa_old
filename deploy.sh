247cb83bcd63ac9d93fc636d8dae73cb3f726722
Ole Martin Gjersvik <olem@olem.org>
2014-02-02 11:52:48 +0100

    Create a snapshot for easy deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,9 @@
 set -u -e;
 
 #Upload client code.
-aws s3 sync ./build s3://ompa.olem.org --acl public-read  --delete
+aws s3 sync ./build s3://ompa.olem.org --acl public-read --delete
+
+#Create a distrebution
+mkdir dist
+dart --shapshot=dist/ompa.snapshot bin/ompa.dart
+ls -a dist
