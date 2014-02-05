6bc4780f270e7a4b8391e353a132da2ad633dc63
Ole Martin Gjersvik <olem@olem.org>
2014-02-05 15:19:19 +0100

    Base64ed cloude-config

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,11 @@ tar -cvf ompa.tar ompa/
 aws s3 cp ompa.tar s3://ompa.olem.org/dist/ompa-$CI_BUILD_NUMBER.tar --acl private
 
 #Create Cloude Config
-cloudconfig=$(sed -e 's/BUILD_NUMBER/$CI_BUILD_NUMBER/g' -e 's|MONGO_URI|$MONGO_URI|g' <cloud-config.yaml)
+cloudconfig=$(sed -e "s/BUILD_NUMBER/$CI_BUILD_NUMBER/g" -e "s|MONGO_URI|$MONGO_URI|g" <cloud-config.yaml)
 echo $cloudconfig
+cloudconfig64=$($cloudconfig | base64 -w 0)
+echo $cloudconfig64
+
 
 #Start new server
 
