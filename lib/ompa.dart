50e4f3c9f07423044adb29dd3187bbe4511773ee
Ole Martin Gjersvik <olem@olem.org>
2014-02-11 18:00:02 +0100

    Moved auth clode to library.

--- lib/ompa.dart
+++ lib/ompa.dart
@@ -3,7 +3,9 @@ library ompa_common;
 import 'dart:convert';
 
 import 'package:bson/bson.dart';
+import 'package:crypto/crypto.dart';
 
+part 'src/auth.dart';
 part 'src/model.dart';
 part 'src/model_factory.dart';
 part 'src/success.dart';
---
+++ lib/src/auth.dart
@@ -0,0 +1,44 @@
+part of ompa_common;
+
+class Auth{
+  
+  final List<int> _key;
+  Auth(this._key);
+  factory Auth.fromPassword(String password){
+    const salt = '5Yc8GDdmKxlYpLnzcGBKpxZ0YU1rottDYGsWbDJrTK4WBsp2Hzd1sOSjAOLPdBfc';
+    var hash = new SHA256();
+    hash.add("$password$salt".codeUnits);
+    return new Auth(hash.close());
+  }
+  
+  factory Auth.fromBase64(String key){
+    return new Auth(CryptoUtils.base64StringToBytes(key));
+  }
+  
+  String sign({String path: '/', 
+               String body: '',
+               String metode: 'GET'}){
+    var key = CryptoUtils.bytesToBase64(_getHmac(path,body,metode).close());
+    return 'OMPA-TOKEN token="$key"';
+  }
+  
+  bool validate(String authHeader, {String path: '/', 
+                 String body: '',
+                 String metode: 'GET'}){
+    
+    var digest = CryptoUtils.base64StringToBytes(authHeader.split('"')[1]);
+    return _getHmac(path,body,metode).verify(digest);
+  }
+  
+  String toBase64(){
+    return CryptoUtils.bytesToBase64(_key);
+  }
+  
+  HMAC _getHmac(path, body, method){
+    var hmac = new HMAC(new SHA256(),_key);
+    hmac.add(path.codeUnits);
+    hmac.add(method.codeUnits);
+    hmac.add(body.codeUnits);
+    return hmac;
+  }
+}
