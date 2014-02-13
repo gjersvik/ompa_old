78efc6bd2794282dcc1a7b1042e8d7be02b21bfd
Ole Martin Gjersvik <olem@olem.org>
2014-02-13 17:03:23 +0100

    Coments to echo.

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 set -u -e;
 
-#Get aws utils
+echo "Get aws utils"
 pip -q install awscli
 
-#Upload client code.
+echo "Upload client code."
 aws s3 sync ./build s3://ompa.olem.org --acl public-read
 
-#Create a distrebution
+echo "Create a server distrebution."
 mkdir ompa
 dart --snapshot=ompa/ompa.snapshot bin/ompa.dart
 cp bin/start.sh ompa/start.sh
 tar -cvf ompa.tar ompa/
 
-#Upload distrebution.
+echo "Upload distrebution."
 aws s3 cp ompa.tar s3://ompa.olem.org/dist/ompa-$CI_BUILD_NUMBER.tar --acl private
 
-#Create Cloude Config
+echo "Create Cloude Config".
 cloudconfig=$(sed -e "s/BUILD_NUMBER/$CI_BUILD_NUMBER/g" -e "s|MONGO_URI|$MONGO_URI|g" <cloud-config.yaml)
 echo $cloudconfig
 cloudconfig64=$(echo $cloudconfig | base64 -w 0)
 echo $cloudconfig64
 
+echo "Start new server"
 
-#Start new server
+echo "Wait for server"
 
-#Wait for server
+echo "Update DNS"
 
-#Update DNS
-
-
-
-#Nuke old server
+echo "Nuke old server"
