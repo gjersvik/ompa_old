2b3976d6a7697b756ff1b5b64785be00eaafcd18
Ole Martin Gjersvik <olem@olem.org>
2014-02-15 21:25:04 +0100

    Fixed role name.

--- deploy.sh
+++ deploy.sh
@@ -17,13 +17,14 @@ echo "Upload distrebution."
 aws s3 cp ompa.tar s3://ompa.olem.org/dist/ompa-$CI_BUILD_NUMBER.tar --acl private
 
 echo "Create Cloude Config".
-cloudconfig=$(sed -e "s/BUILD_NUMBER/$CI_BUILD_NUMBER/g" -e "s|MONGO_URI|$MONGO_URI|g" <cloud-config.yaml)
+cloudconfig=$(sed -e "s/BUILD_NUMBER/$CI_BUILD_NUMBER/g" \
+-e "s|MONGO_URI|$MONGO_URI|g" <cloud-config.yaml)
 echo "$cloudconfig"
 
 echo "Start new server"
 aws iam create-instance-profile --instance-profile-name ompa-$CI_BUILD_NUMBER
 aws iam add-role-to-instance-profile \
---instance-profile-name ompa-$CI_BUILD_NUMBER --role-name ompa-server-s3
+--instance-profile-name ompa-$CI_BUILD_NUMBER --role-name ompa-server
 
 aws ec2 run-instances --image-id ami-0e0cf879 --security-groups Ompa \
 --user-data "$cloudconfig" --instance-type t1.micro --key-name OleMartin \
