80411794dcb2292f367315a63ceb0df6117b4a7d
Ole Martin Gjersvik <olem@olem.org>
2014-02-16 13:16:06 +0100

    It shuld start a server now.

--- bin/start.sh
+++ bin/start.sh
@@ -3,4 +3,4 @@ set -u -e
 echo $1
 wget http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/dartsdk-linux-x64-release.zip
 unzip dartsdk-linux-x64-release.zip
-#./dart-sdk/bin/dart ompa.dart $1
+./dart-sdk/bin/dart ompa.dart $1
