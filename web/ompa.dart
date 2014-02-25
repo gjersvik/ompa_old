7a5e6f12b339b252c612725b1565d8e8822b17f2
Ole Martin Gjersvik <olem@olem.org>
2014-02-25 14:45:55 +0100

    Moved auth code to auth controller.

--- web/ompa.dart
+++ web/ompa.dart
@@ -11,9 +11,7 @@ import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:ompa/ompa.dart';
 
-part 'src/auth.dart';
 part 'src/auth_controller.dart';
-part 'src/auth_panel.dart';
 part 'src/panel.dart';
 part 'src/panels.dart';
 part 'src/new_note_panel.dart';
@@ -26,14 +24,14 @@ part 'src/tasks.dart';
 
 void main() {
   var ompaModule = new Module();
-  var authctrl = new AuthController();
-  ompaModule.value(AuthController,authctrl);
+  ompaModule.type(AuthController);
+  Injector injector = ngBootstrap(module: ompaModule);
   
-  ngBootstrap(module: ompaModule);
+  AuthController authctrl =  injector.get(AuthController);
   
   Panels panels = new Panels();
   document.body.append(panels.elem);
-  auth(panels).then((auth){
+  authctrl.onAuth.then((auth){
     var server;
     if(window.location.host == '127.0.0.1:3030'){
       server = new Server('http://127.0.0.1:8080/',auth);
--- web/src/auth_controller.dart
+++ web/src/auth_controller.dart
@@ -5,14 +5,35 @@ class AuthController{
   bool loggedIn = false;
   String key;
   
+  Future<Auth> onAuth;
+  
+  // HACK
+  static Completer<Auth> _onAuth =  new Completer<Auth>();
+  
+  
   AuthController() {
+    onAuth = _onAuth.future;
+    // HACK
+    if(_onAuth.isCompleted){
+      return;
+    }
+      
+    if(window.sessionStorage.containsKey('passhash')){
+      _onAuth.complete(new Auth.fromBase64(window.sessionStorage['passhash']));
+      loggedIn = true;
+    }
   }
   
   login(){
+    var auth = new Auth.fromPassword(key);
+    _onAuth.complete(auth);
+    window.sessionStorage['passhash'] = auth.toBase64();
     loggedIn = true;
   }
   
   logout(){
     loggedIn = false;
+    window.sessionStorage.remove('passhash');
+    window.location.assign(window.location.href);
   }
 }
