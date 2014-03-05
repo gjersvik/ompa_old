795bf0004b59bdd0f732c21be3ee100afb042f63
Ole Martin Gjersvik <olem@olem.org>
2014-03-05 09:56:28 +0100

    New route note.

--- web/src/route.dart
+++ web/src/route.dart
@@ -2,8 +2,11 @@ part of ompa_html;
 
 void ompaRouteInitializer(Router router, ViewFactory views) {
   views.configure({
-    'add': ngRoute(
+    'success': ngRoute(
         path: '/success',
-        view: 'view/success.html')
+        view: 'view/success.html'),
+    'note': ngRoute(
+        path: '/note',
+        view: 'view/note.html')
   });
 }
