f0a2b35b00ceee071f311b43a528173d8bd9efa2
Ole Martin Gjersvik <olem@olem.org>
2014-03-05 11:38:15 +0100

    Fixed auth.

--- web/src/auth_controller.dart
+++ web/src/auth_controller.dart
@@ -8,6 +8,7 @@ class AuthController{
   AuthService _service;
   
   AuthController(AuthService this._service) {
+    loggedIn = _service.auth != null;
     _service.onChange.listen((Auth key)=>loggedIn = key != null);
   }
   
