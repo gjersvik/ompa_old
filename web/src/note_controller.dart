31c8306352741a63d63acd7c4cd4cb8c0670a167
Ole Martin Gjersvik <olem@olem.org>
2014-03-07 14:29:20 +0100

    Note sort is done.

--- web/src/note_controller.dart
+++ web/src/note_controller.dart
@@ -11,12 +11,39 @@ class NoteController{
     _service.getAll().then((List n) => notes = n);
   }
   
-  save(Note note, int index){
-    note.sort = index;
+  save(Note note){
     _service.save(note);
   }
   
   delete(Note note) => _service.remove(note).then(notes.remove);
+  
+  prev(Note note){
+    if(note == notes.first){
+      return;
+    }
+    int i = notes.indexOf(note);
+    swap(i, i - 1);
+  }
+  
+  next(Note note){
+    if(note == notes.last){
+      return;
+    }
+    int i = notes.indexOf(note);
+    swap(i, i + 1);
+  }
+  
+  swap(int i1, int i2){
+    notes[i1].sort = i2;
+    notes[i2].sort = i1;
+    
+    var temp = notes[i1];
+    notes[i1] = notes[i2];
+    notes[i2] = temp;
+    
+    save(notes[i1]);
+    save(notes[i2]);
+  }
   
   add(){
     var note = new Note();
