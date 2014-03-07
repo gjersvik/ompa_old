98de9ae707f907aa1d3ddfb93c418fb897f6f5c1
Ole Martin Gjersvik <olem@olem.org>
2014-03-07 11:07:40 +0100

    Changed note.

--- lib/src/note.dart
+++ lib/src/note.dart
@@ -2,7 +2,10 @@ part of ompa_common;
 
 class Note extends Model{
   String name = '';
+  int sort = 0;
   String text = '';
+  
+  ObjectId _id = new ObjectId();
   
   Note();
 
@@ -13,26 +16,40 @@ class Note extends Model{
     var model = new Note();
     model.name = json['name'];
     model.text = json['text'];
+    model._id = new ObjectId.fromHexString(json['_id']);
+    model.sort = json['sort']; 
     return model;
   }
   
   factory Note.fromDb(Map data){
     var model = new Note();
-    model.name = data['_id'];
+    if(data.containsKey('name')){
+      model._id = data['_id'];
+      model.name = data['name'];
+    }else{
+      model.name = data['_id'];
+    }
     model.text = data['text'];
+    if(data.containsKey('sort')){
+      model.sort = data['sort'];
+    }
     return model;
   }
   
   Map toJson(){
     return {
+      '_id': _id.toHexString(),
       'name': name,
+      'sort': sort,
       'text': text
     };
   }
   
   Map toDb(){
     return {
-      '_id': name,
+      '_id': _id,
+      'name': name,
+      'sort': sort,
       'text': text
     };
   }
