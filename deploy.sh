ac12048c4f99409c30707376d4eab1e1fca29de2
Ole Martin Gjersvik <olem@olem.org>
2014-05-10 20:10:08 +0200

    A new try on zip and new version.

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ aws s3 sync ./build s3://ompa.olem.org --quiet --acl public-read
 
 echo "Create a server distrebution."
 mkdir ./ompa
-cp -r ./bin ./ompa
-zip -r ompa.zip ./ompa Dockerfile Dockerfile.aws.json
+cp -rL ./bin/* ./ompa
+zip -r ompa.zip ./ompa Dockerfile Dockerrun.aws.json
 
 echo "Upload distrebution."
 aws s3 cp ompa.zip s3://ompa.olem.org/dist/ompa-$CI_BUILD_NUMBER.zip --acl private
 
-aws elastic-beanstalk-create-application-version -a ompa -d $CI_BUILD_NUMBER  -s ompa.olem.org/dist/ompa-$CI_BUILD_NUMBER.zip
+aws elasticbeanstalk create-application-version --application-name ompa --version-label $CI_BUILD_NUMBER  --source-bundle S3Bucket=ompa.olem.org,S3Key=dist/ompa-$CI_BUILD_NUMBER.zip
