45d0f4418d29e3567316ee74402a8c7462f52105
Ole Martin Gjersvik <olem@olem.org>
2014-05-10 19:17:44 +0200

    A little zip to tar bug.

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,4 @@ cp -r ./bin ./ompa
 zip -r ompa.zip ./ompa Dockerfile Dockerfile.aws.json
 
 echo "Upload distrebution."
-aws s3 cp ompa.tar s3://ompa.olem.org/dist/ompa-$CI_BUILD_NUMBER.tar --acl private
+aws s3 cp ompa.zip s3://ompa.olem.org/dist/ompa-$CI_BUILD_NUMBER.zip --acl private
