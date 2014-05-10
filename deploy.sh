6e179da56d127ca9fdecbf9856643ac85dac5bf9
Ole Martin Gjersvik <olem@olem.org>
2014-05-10 19:49:58 +0200

    Add version to beanstack

--- deploy.sh
+++ deploy.sh
@@ -14,3 +14,5 @@ zip -r ompa.zip ./ompa Dockerfile Dockerfile.aws.json
 
 echo "Upload distrebution."
 aws s3 cp ompa.zip s3://ompa.olem.org/dist/ompa-$CI_BUILD_NUMBER.zip --acl private
+
+aws elastic-beanstalk-create-application-version -a ompa -d $CI_BUILD_NUMBER  -s ompa.olem.org/dist/ompa-$CI_BUILD_NUMBER.zip
